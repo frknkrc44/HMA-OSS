6d9706437c905d188bde95416fb0c9c21ddc8c89
frknkrc44 <krc440002@gmail.com>
2026-01-13 16:35:44 +0300

    Merge new changes
    - New Crowdin updates (#59)
    - Revamped doc layout (#60 - Neebe3289)
    - Merge oss-next changes (#61)
      - Upload translators.json for RB compatibility
      - Add cache for presets (it will decrease the boot time)
      - Add threads for presets again (I assume it was required from beginning)
      - Other fixes and improvements
    - Add more Detectors (#62 - Yurii0307)
    - Remove animations when it's disabled in system settings (#63 - stevenxxiu)
    
    Co-authored-by: Neebe3289 <neebexd@gmail.com>
    Co-authored-by: Yurii0307 <nguyenvovantai1@gmail.com>
    Co-authored-by: Steven Xu <stevenxxiu@users.noreply.github.com>

--- app/src/main/java/icu/nullptr/hidemyapplist/ui/util/AccessibilityUtils.kt
+++ app/src/main/java/icu/nullptr/hidemyapplist/ui/util/AccessibilityUtils.kt
@@ -1,14 +1,17 @@
 package icu.nullptr.hidemyapplist.ui.util
 
 import android.content.ContentResolver
-import android.provider.Settings
+import android.provider.Settings.Global.ANIMATOR_DURATION_SCALE
+import android.provider.Settings.Global.TRANSITION_ANIMATION_SCALE
+import android.provider.Settings.Global.WINDOW_ANIMATION_SCALE
+import android.provider.Settings.Global.getFloat
 
 class AccessibilityUtils {
     companion object {
         fun isAnimationEnabled(cr: ContentResolver): Boolean {
-            return !(Settings.Global.getFloat(cr, Settings.Global.ANIMATOR_DURATION_SCALE, 1.0f) == 0.0f
-                && Settings.Global.getFloat(cr, Settings.Global.TRANSITION_ANIMATION_SCALE, 1.0f) == 0.0f
-                && Settings.Global.getFloat(cr, Settings.Global.WINDOW_ANIMATION_SCALE, 1.0f) == 0.0f)
+            return getFloat(cr, ANIMATOR_DURATION_SCALE, 1.0f) > 0.0f
+                && getFloat(cr, TRANSITION_ANIMATION_SCALE, 1.0f) > 0.0f
+                && getFloat(cr, WINDOW_ANIMATION_SCALE, 1.0f) > 0.0f
         }
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ val appVerName by extra("oss-${gitCommitCountAfterOss}${gitHasUncommittedSuffix}
 val configVerCode by extra(93)
 
 @Suppress("unused")
-val serviceVerCode by extra(101)
+val serviceVerCode by extra(102)
 
 @Suppress("unused")
 val minBackupVerCode by extra(65)
