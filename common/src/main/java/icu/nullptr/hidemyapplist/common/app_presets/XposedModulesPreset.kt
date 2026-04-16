a2218125bdc872bb0665aba7177b2a67f7d93398
frknkrc44 <krc440002@gmail.com>
2026-04-16 15:32:55 +0300

    Revert Xposed modules preset changes

--- common/src/main/java/icu/nullptr/hidemyapplist/common/app_presets/XposedModulesPreset.kt
+++ common/src/main/java/icu/nullptr/hidemyapplist/common/app_presets/XposedModulesPreset.kt
@@ -2,16 +2,13 @@ package icu.nullptr.hidemyapplist.common.app_presets
 
 import android.content.pm.ApplicationInfo
 import icu.nullptr.hidemyapplist.common.Utils.checkSplitPackages
-import org.frknkrc44.hma_oss.common.BuildConfig
 
 class XposedModulesPreset : BasePreset(NAME) {
     companion object {
         const val NAME = "xposed"
     }
 
-    override val exactPackageNames = setOf(
-        BuildConfig.APP_PACKAGE_NAME,
-    )
+    override val exactPackageNames = setOf<String>()
 
     override fun canBeAddedIntoPreset(appInfo: ApplicationInfo): Boolean {
         return checkSplitPackages(appInfo) { _, zipFile ->
