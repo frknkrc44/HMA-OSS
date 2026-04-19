79ff3d96036cd65a88524d9e91934caa19ce882b
frknkrc44 <krc440002@gmail.com>
2026-04-19 13:32:56 +0300

    Add "Nuko" app icon by OukaroMF

--- app/src/main/java/icu/nullptr/hidemyapplist/data/AppConstants.kt
+++ app/src/main/java/icu/nullptr/hidemyapplist/data/AppConstants.kt
@@ -8,12 +8,14 @@ object AppConstants {
     private const val COMPONENT_NAME_ALT     = "${BuildConfig.APPLICATION_ID}.MainActivityLauncherAlt"
     private const val COMPONENT_NAME_ALT_2   = "${BuildConfig.APPLICATION_ID}.MainActivityLauncherAlt2"
     private const val COMPONENT_NAME_ALT_3   = "${BuildConfig.APPLICATION_ID}.MainActivityLauncherAlt3"
+    private const val COMPONENT_NAME_ALT_4   = "${BuildConfig.APPLICATION_ID}.MainActivityLauncherAlt4"
 
     val allAppIcons = listOf(
         R.mipmap.ic_launcher       to COMPONENT_NAME_DEFAULT,
         R.mipmap.ic_launcher_alt   to COMPONENT_NAME_ALT,
         R.mipmap.ic_launcher_alt_2 to COMPONENT_NAME_ALT_2,
         R.mipmap.ic_launcher_alt_3 to COMPONENT_NAME_ALT_3,
+        R.mipmap.ic_launcher_alt_5 to COMPONENT_NAME_ALT_4,
     )
 
     const val UPDATE_CHECK_URL = "https://api.github.com/repos/frknkrc44/HMA-OSS/releases/latest"
