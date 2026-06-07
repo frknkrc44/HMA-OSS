79b953c2601a11eea7e428c30e0fc468426bfa21
frknkrc44 <krc440002@gmail.com>
2026-06-07 18:49:17 +0300

    Detect APatch and Magisk Zygisk

--- zygote/src/main/assets/customize.d/21-enforce-ksu-kernel.sh
+++ zygote/src/main/assets/customize.d/21-enforce-ksu-kernel.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-# Enforce kernelsu kernel and manager version matched and zygisk framework is installed
+# Enforce KernelSU kernel and manager versions matched
 
 if [ "$KSU" ]; then
     # get language
@@ -10,72 +10,21 @@ if [ "$KSU" ]; then
 
     # language pack
     if echo "$SYSTEM_LANG" | grep -q "zh"; then
-        KSU_DETECTED_MSG="- 检测到 KernelSU"
         KSU_VERSION_MISMATCH_WARN="! 警告: KernelSU 管理器 ($KSU_VER_CODE) 与内核 ($KSU_KERNEL_VER_CODE) 版本不匹配! HMA-OSS 不对由此引发的系统异常（如: 卡米）负责，且模块可能无法激活!"
         INSTALLER_CONTINUE_MSG(){
             echo "- 安装将在 $1 秒后继续"
         }
-        ZYGISK_DETECTED_MSG(){
-            echo "- 检测到 $1 框架"
-        }
-        ZYGISK_DISABLED_ERR(){
-            echo "! $1 已禁用, HMA-OSS 需要 Zygisk 才能正常运行, 安装程序已退出"
-        }
-        ZYGISK_REMOVED_ERR(){
-            echo "! $1 已卸载, HMA-OSS 需要 Zygisk 才能正常运行, 安装程序已退出"
-        }
-        ZYGISK_MULTI_ERR="! 检测到多个 Zygisk 框架, 为了避免冲突, 安装程序已退出"
-        ZYGISK_NOT_FOUND_ERR="! 未找到已知的 Zygisk 框架 (例如 ZygiskNext), HMA-OSS 需要 Zygisk 才能正常运行, 安装程序已退出"
     else
-        KSU_DETECTED_MSG="- KernelSU found."
         KSU_VERSION_MISMATCH_WARN="! KernelSU manager version ($KSU_VER_CODE) does not match kernel version ($KSU_KERNEL_VER_CODE). HMA-OSS not take any responsibilities for incompatibilities!"
         INSTALLER_CONTINUE_MSG(){
             echo "- The installer will continue in $1 seconds"
         }
-        ZYGISK_DETECTED_MSG(){
-            echo "- Found $1 framework."
-        }
-        ZYGISK_DISABLED_ERR(){
-            echo "! $1 is disabled, HMA-OSS requires Zygisk to work."
-        }
-        ZYGISK_REMOVED_ERR(){
-            echo "! $1 is removed, HMA-OSS requires Zygisk to work."
-        }
-        ZYGISK_MULTI_ERR="! Multiple Zygisk frameworks were found. Aborting installation to prevent conflicts."
-        ZYGISK_NOT_FOUND_ERR="! No known Zygisk frameworks (e.g. ZygiskNext) is found, HMA-OSS requires Zygisk to work. Installation aborted."
     fi
-    ui_print "$KSU_DETECTED_MSG"
+
     # kernel and manager version mismatch
     if [ "$KSU_VER_CODE" != "$KSU_KERNEL_VER_CODE" ]; then
         ui_print "$KSU_VERSION_MISMATCH_WARN"
         ui_print "$(INSTALLER_CONTINUE_MSG 5)"
         sleep 5
-    fi
-    find_zygisk(){
-        if [ -d "/data/adb/modules/$1" ] || [ -d "/data/adb/modules_update/$1" ]; then
-            [ -f "/data/adb/modules/$1/disable" ] && return
-            [ -f "/data/adb/modules/$1/remove" ] && return
-
-            [ ! -z "$ZYGISK_ID" ] && abort "$ZYGISK_MULTI_ERR"
-
-            ZYGISK_ID="$1"
-            ZYGISK_NAME="$2"
-        fi
-    }
-    # add known zygisk frameworks here...
-    find_zygisk "zygisksu" "ZygiskNext / NeoZygisk"
-    find_zygisk "rezygisk" "ReZygisk"
-    find_zygisk "admirepowered" "Zygisk Mod"
-    find_zygisk "zygisk_on_ksu" "Zygisk on KernelSU"
-
-    # not installed zygisk
-    if [ -z "$ZYGISK_ID" ]; then
-        abort "$ZYGISK_NOT_FOUND_ERR"
-    elif [ -f "/data/adb/modules/$ZYGISK_ID/disable" ]; then
-        abort "$(ZYGISK_DISABLED_ERR "$ZYGISK_NAME")"
-    elif [ -f "/data/adb/modules/$ZYGISK_ID/remove" ]; then
-        abort "$(ZYGISK_REMOVED_ERR "$ZYGISK_NAME")"
-    else
-        ui_print "$(ZYGISK_DETECTED_MSG "$ZYGISK_NAME")"
     fi
 fi
---
+++ zygote/src/main/assets/customize.d/22-check-zygisk.sh
@@ -0,0 +1,75 @@
+#!/system/bin/sh
+
+# check for a Zygisk framework was installed and enabled
+
+# get language
+[ -z "$SYSTEM_LANG" ] && SYSTEM_LANG=$(getprop persist.sys.locale 2>/dev/null)
+[ -z "$SYSTEM_LANG" ] && SYSTEM_LANG=$(getprop persist.sys.language 2>/dev/null)
+[ -z "$SYSTEM_LANG" ] && SYSTEM_LANG=$(settings get system system_locales 2>/dev/null)
+
+# language pack
+if echo "$SYSTEM_LANG" | grep -q "zh"; then
+    ZYGISK_DETECTED_MSG(){
+        echo "- 检测到 $1 框架"
+    }
+    ZYGISK_DISABLED_ERR(){
+        echo "! $1 已禁用, HMA-OSS 需要 Zygisk 才能正常运行, 安装程序已退出"
+    }
+    ZYGISK_REMOVED_ERR(){
+        echo "! $1 已卸载, HMA-OSS 需要 Zygisk 才能正常运行, 安装程序已退出"
+    }
+    ZYGISK_MULTI_ERR="! 检测到多个 Zygisk 框架, 为了避免冲突, 安装程序已退出"
+    ZYGISK_NOT_FOUND_ERR="! 未找到已知的 Zygisk 框架 (例如 ZygiskNext), HMA-OSS 需要 Zygisk 才能正常运行, 安装程序已退出"
+else
+    ZYGISK_DETECTED_MSG(){
+        echo "- Found $1 framework"
+    }
+    ZYGISK_DISABLED_ERR(){
+        echo "! $1 is disabled, HMA-OSS requires Zygisk to work"
+    }
+    ZYGISK_REMOVED_ERR(){
+        echo "! $1 is removed, HMA-OSS requires Zygisk to work"
+    }
+    ZYGISK_MULTI_ERR="! Multiple Zygisk frameworks were found. Aborting installation to prevent conflicts"
+    ZYGISK_NOT_FOUND_ERR="! No known Zygisk frameworks (e.g. ZygiskNext) is found, HMA-OSS requires Zygisk to work. Installation aborted"
+fi
+
+find_zygisk(){
+    if [ -d "/data/adb/modules/$1" ] || [ -d "/data/adb/modules_update/$1" ]; then
+        [ -f "/data/adb/modules/$1/disable" ] && return
+        [ -f "/data/adb/modules/$1/remove" ] && return
+
+        [ ! -z "$ZYGISK_ID" ] && abort "$ZYGISK_MULTI_ERR"
+
+        ZYGISK_ID="$1"
+        ZYGISK_NAME="$2"
+    fi
+}
+
+if ! ([ "$KSU" ] || [ "$APATCH" ])
+then
+    MAGISK_ZYGISK=$(magisk --sqlite "SELECT value FROM settings WHERE key = 'zygisk'" 2> /dev/null | cut -f2 -d=)
+
+    if [ "$MAGISK_ZYGISK" == "1" ]
+    then
+        ZYGISK_ID="magisk"
+        ZYGISK_NAME="Magisk Built-in Zygisk"
+    fi
+fi
+
+# add known zygisk frameworks here...
+find_zygisk "zygisksu" "ZygiskNext / NeoZygisk"
+find_zygisk "rezygisk" "ReZygisk"
+find_zygisk "admirepowered" "Zygisk Mod"
+find_zygisk "zygisk_on_ksu" "Zygisk on KernelSU"
+
+# not installed zygisk
+if [ -z "$ZYGISK_ID" ]; then
+    abort "$ZYGISK_NOT_FOUND_ERR"
+elif [ -f "/data/adb/modules/$ZYGISK_ID/disable" ]; then
+    abort "$(ZYGISK_DISABLED_ERR "$ZYGISK_NAME")"
+elif [ -f "/data/adb/modules/$ZYGISK_ID/remove" ]; then
+    abort "$(ZYGISK_REMOVED_ERR "$ZYGISK_NAME")"
+else
+    ui_print "$(ZYGISK_DETECTED_MSG "$ZYGISK_NAME")"
+fi
