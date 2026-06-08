df1ec85eba810bcf6e9f082d1b9eeff9514a8e0f
frknkrc44 <krc440002@gmail.com>
2026-06-08 12:51:47 +0300

    Do not install for user 999

--- zygote/src/main/assets/customize.d/35-install-manager-app.sh
+++ zygote/src/main/assets/customize.d/35-install-manager-app.sh
@@ -5,7 +5,7 @@ APK_FILE=$MODPATH/manager.apk
 install_pkg() {
   pm install --user $1 $APK_FILE 2>&1
 
-  [ $? -ne 0 ] && abort "! Cannot install the manager app"
+  [ $? -ne 0 ] && ui_print "! Cannot install the manager app for user "$user
 }
 
 # we need to determine the manager app was installed or not at first
@@ -14,8 +14,8 @@ then
   # determine the installed user
   for user in $(pm list users | cut -f1 -d: | cut -f2 -d{ | tail -n +2)
   do
-    # if path detected in user then install the manager app for it
-    if pm path --user $user org.frknkrc44.hma_oss &> /dev/null
+    # if path detected in user then install the manager app for it (except Xiaomi's dual app space)
+    if [ "$user" != "999" ] && pm path --user $user org.frknkrc44.hma_oss &> /dev/null
     then
       ui_print "- Installing manager app update for user "$user
 
