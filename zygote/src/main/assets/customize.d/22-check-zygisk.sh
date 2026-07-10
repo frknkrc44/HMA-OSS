04fb4d646b22ec5f851b66df78b298f82136f632
frknkrc44 <krc440002@gmail.com>
2026-07-10 10:21:27 +0300

    Remove YukiSU check from the Zygisk framework checker

--- zygote/src/main/assets/customize.d/22-check-zygisk.sh
+++ zygote/src/main/assets/customize.d/22-check-zygisk.sh
@@ -42,9 +42,9 @@ find_zygisk "rezygisk" "ReZygisk"
 find_zygisk "admirepowered" "Zygisk Mod"
 find_zygisk "zygisk_on_ksu" "Zygisk on KernelSU"
 
-if [ -z "$ZYGISK_NAME" ] && ([ "$ZYGISK_ENABLED" == "1" ] || [ "$YUKISU" ])
+if [ -z "$ZYGISK_NAME" ] && [ "$ZYGISK_ENABLED" == "1" ]
 then
-   ZYGISK_NAME="$FALLBACK_ZYGISK_NAME"
+    ZYGISK_NAME="$FALLBACK_ZYGISK_NAME"
 fi
 
 # not installed zygisk
