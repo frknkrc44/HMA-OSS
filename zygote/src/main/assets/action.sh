1fe6c8b87a0bdbab2838223fd25cc15aecacef74
Nờ Lê Lộc <141159425+nleloc@users.noreply.github.com>
2026-08-09 17:58:27 +0000

    feat: add module working status in its description
    Modified: `action.sh`: make action button renew module status (still able to launch the manager, only launch for user 0)
    
    New files:
    `40-setup-module-status.sh`: backup original module.prop, move `hmaoss.sh` to `post-fs-data.d` and `post-mount.d` (for best compatibility)
    `hmaoss.sh`: reset HMA-OSS's module.prop even if HMA-OSS is disabled
    `service.sh`: waits for HMA-OSS's status file and trigger update_desc.sh
    `uninstall.sh`: remove script(s) when uninstalling

--- zygote/src/main/assets/action.sh
+++ zygote/src/main/assets/action.sh
@@ -1,3 +1,12 @@
 #!/system/bin/sh
 
-am start org.frknkrc44.hma_oss/org.frknkrc44.hma_oss.ui.activity.MainActivity
+MODDIR="${0%/*}"
+
+# launch manager first
+echo "Launching HMA-OSS on user 0"
+am start -n org.frknkrc44.hma_oss/org.frknkrc44.hma_oss.ui.activity.MainActivity --user 0
+
+# reload module status
+# NOTE: `update_desc.sh` already replace whole description= line so i don't think re-copy is neccessary
+sh "$MODDIR/update_desc.sh"
+echo "Updated module status"
