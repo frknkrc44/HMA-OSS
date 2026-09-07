9dc63d259318bd6e4d8319dd3e398a1201aae9a5
nleloc <141159425+nleloc@users.noreply.github.com>
2026-09-07 09:25:12 +0700

    fix(action.sh): stop hardcoding --user 0, add manager installation fallback
    - Reuse profile-detection logic in 35-install-manager-app.sh
    - Fallback to install manager for user 0 when can't found it on any profile

--- zygote/src/main/assets/action.sh
+++ zygote/src/main/assets/action.sh
@@ -1,12 +1,34 @@
 #!/system/bin/sh
 
 MODDIR="${0%/*}"
+PKG=org.frknkrc44.hma_oss
+ACTIVITY=org.frknkrc44.hma_oss.ui.activity.MainActivity
+APK_FILE="$MODDIR/manager.apk"
 
-# launch manager first
-echo "Launching HMA-OSS on user 0"
-am start -n org.frknkrc44.hma_oss/org.frknkrc44.hma_oss.ui.activity.MainActivity --user 0
-
-# reload module status
-# NOTE: `update_desc.sh` already replace whole description= line so i don't think re-copy is neccessary
+# reload module status first (regardless of launch outcome)
 sh "$MODDIR/update_desc.sh"
-echo "Updated module status"
+echo "- Updated module status"
+
+install_pkg() {
+  pm install --user $1 $APK_FILE 2>&1
+
+  [ $? -ne 0 ] && echo "! Cannot install the manager app for user "$1
+}
+
+launch_pkg() {
+  echo "- Launching HMA-OSS manager on user "$1
+  am start -n $PKG/$ACTIVITY --user $1
+}
+
+for user in $(pm list users | cut -f1 -d: | cut -f2 -d{ | tail -n +2)
+  do
+    # if path detected in user then install the manager app for it (except Xiaomi's dual app space)
+    if [ "$user" != "999" ] && pm path --user $user $PKG &> /dev/null
+    then
+      launch_pkg $user
+      exit 0
+    fi
+done
+
+echo "- Manager app not found, installing for user 0"
+install_pkg 0 && launch_pkg 0
