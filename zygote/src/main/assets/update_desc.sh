c58500aca66b4d4ae4cb022c62a69c235982ece3
frknkrc44 <krc440002@gmail.com>
2026-09-16 14:23:33 +0300

    Try to improve update_desc script

--- zygote/src/main/assets/update_desc.sh
+++ zygote/src/main/assets/update_desc.sh
@@ -9,10 +9,12 @@ ORIG_DESC_FIX=$(printf '%s\n' "$ORIG_DESC" | sed 's/[&/\]/\\&/g')
 
 STATUS_FILE=$(printf '%s' /data/misc/hide_my_applist_*/status.json)
 
+echo "Status file: $STATUS_FILE"
+
 if [ -z "$STATUS_FILE" ] || [ ! -s "$STATUS_FILE" ]; then
-    MODE=""
+    MODE="0"
 else
-    MODE=$(sed -n 's/.*"workMode"[[:space:]]*:[[:space:]]*\([0-9]+\).*/\1/p' "$STATUS_FILE")
+    MODE=$(grep -o '"workMode":[0-9]*' $STATUS_FILE | cut -f2 -d:)
 fi
 
 case "$MODE" in
@@ -22,5 +24,7 @@ case "$MODE" in
     4) STATUS="[❌ System service crashed]"  ;;
     *) STATUS="[❓ Unknown]" ;;
 esac
+
+echo "Detected status: $STATUS"
 
 sed -i "s/^description=.*/description=$STATUS $ORIG_DESC_FIX/" "$MODDIR/module.prop"
